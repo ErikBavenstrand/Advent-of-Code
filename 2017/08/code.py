a0fab1c3a0ea3cf21a4d4c4b8286cd6d6750d15b
Erik Båvenstrand <erik.bavenstrands@gmail.com>
2021-12-05 09:46:59 +0100

    Changed naming of fodlers for sorting

--- 2017/08/code.py
+++ 2017/08/code.py
@@ -1,47 +1,6 @@
-import argparse
-import os
-from datetime import date
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--start',
-                    default=2015, type=int)
-parser.add_argument('--end',
-                    default=date.today().year, type=int)
-args = parser.parse_args()
-
-base_dir = "./"
-AUTHOR = "Erik Båvenstrand"  # Name automatically put in the code templates.
-
-# DATE SPECIFIC PARAMETERS
-years = range(args.start, args.end + 1)
-days = range(1, 26)
-link = "https://adventofcode.com/"
-
-
-def mkdir_if_not_exist(path: str):
-    if not os.path.exists(path):
-        os.mkdir(path)
-    return path
-
-
-print("Setup will create working directories and files for Advent of Code.")
-
-# Create base dir if it doesn't exist
-mkdir_if_not_exist(base_dir)
-
-for y in years:
-    print(f"Year: {str(y)}")
-    year_path = mkdir_if_not_exist(base_dir + str(y))
-    for d in days:
-        if d < 10:
-            d = f"0{d}"
-        day_path = mkdir_if_not_exist(os.path.join(year_path, str(d)))
-
-        if not os.path.exists(os.path.join(day_path, "code.py")):
-            with open(os.path.join(day_path, "code.py"), "w+") as f:
-                f.write(f"""# Advent of Code {str(y)} Day {str(d)}
-# Author: {AUTHOR}
-# URL: https://adventofcode.com/{str(y)}/day/{str(d)}
+# Advent of Code 2017 Day 08
+# Author: Erik Båvenstrand
+# URL: https://adventofcode.com/2017/day/08
 
 import argparse
 import os.path
@@ -61,7 +20,7 @@ if args.testcase:
                             "testcase.txt")), "r") as f:
         data = f.read().splitlines()
 else:
-    data = get_data(day={str(d)}, year={str(y)}).splitlines()
+    data = get_data(day=08, year=2017).splitlines()
 
 ###############################################################################
 # ██████╗  █████╗ ██████╗ ████████╗     ██╗                                   #
@@ -76,7 +35,7 @@ else:
 answer_a = None
 print("Part a: " + str(answer_a))
 if args.submit and not args.testcase and answer_a:
-    submit(answer=answer_a, part="a", day={str(d)}, year={str(y)})
+    submit(answer=answer_a, part="a", day=08, year=2017)
 ###############################################################################
 # ██████╗  █████╗ ██████╗ ████████╗    ██████╗                                #
 # ██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝    ╚════██╗                               #
@@ -90,12 +49,4 @@ if args.submit and not args.testcase and answer_a:
 answer_b = None
 print("Part b: " + str(answer_b))
 if args.submit and not args.testcase and answer_b:
-    submit(answer=answer_b, part="b", day={str(d)}, year={str(y)})
-""")
-
-        if not os.path.exists(os.path.join(day_path, "testcase.txt")):
-            with open(os.path.join(day_path, "testcase.txt"), "w+") as f:
-                pass
-
-print("Setup complete : Advent of Code working directories \
-and files initialized with success.")
+    submit(answer=answer_b, part="b", day=08, year=2017)
