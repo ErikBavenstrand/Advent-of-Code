8fbf96926cdf6c67d4125e602cddb79ebc7fa29f
Erik Båvenstrand <erik.bavenstrands@gmail.com>
2021-12-11 17:55:36 +0100

    2021 01

--- 2021/01/code.py
+++ 2021/01/code.py
@@ -1,74 +1,29 @@
-# Advent of Code 2021 Day 1
+# Advent of Code 2021 Day 01
 # Author: Erik Båvenstrand
 # URL: https://adventofcode.com/2021/day/1
 
-import argparse
-import os.path
+def part_a(data: list[str]):
+    previous_value = float("inf")
+    counter = 0
+    for line in data:
+        current_value = int(line)
+        if previous_value < current_value:
+            counter += 1
+        previous_value = current_value
+    return counter
 
-from aocd import get_data, submit
 
-parser = argparse.ArgumentParser()
-group = parser.add_mutually_exclusive_group()
-group.add_argument("-t, --testcase", dest="testcase", action="store_true")
-group.add_argument("-s, --submit", dest="submit", action="store_true")
-parser.set_defaults(testcase=False, submit=False)
-args = parser.parse_args()
-
-data = ""
-if args.testcase:
-    with open((os.path.join(os.path.dirname(__file__),
-                            "testcase.txt")), "r") as f:
-        data = f.read().splitlines()
-else:
-    data = get_data(day=1, year=2021).splitlines()
-
-###############################################################################
-# ██████╗  █████╗ ██████╗ ████████╗     ██╗                                   #
-# ██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝    ███║                                   #
-# ██████╔╝███████║██████╔╝   ██║       ╚██║                                   #
-# ██╔═══╝ ██╔══██║██╔══██╗   ██║        ██║                                   #
-# ██║     ██║  ██║██║  ██║   ██║        ██║                                   #
-# ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝        ╚═╝                                   #
-###############################################################################
-
-previous_value = float("inf")
-counter = 0
-for current_value in data:
-
-    current_value = int(current_value)
-    if previous_value < current_value:
-        counter += 1
-    previous_value = current_value
-
-answer_a = counter
-print("Part a: " + str(answer_a))
-if args.submit and not args.testcase and answer_a:
-    submit(answer=answer_a, part="a", day=1, year=2021)
-###############################################################################
-# ██████╗  █████╗ ██████╗ ████████╗    ██████╗                                #
-# ██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝    ╚════██╗                               #
-# ██████╔╝███████║██████╔╝   ██║        █████╔╝                               #
-# ██╔═══╝ ██╔══██║██╔══██╗   ██║       ██╔═══╝                                #
-# ██║     ██║  ██║██║  ██║   ██║       ███████╗                               #
-# ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝       ╚══════╝                               #
-###############################################################################
-
-previous_value = float("inf")
-window = [float("inf"), float("inf"), float("inf")]
-window_idx = 0
-counter = 0
-for current_value in data:
-    current_value = int(current_value)
-
-    window[window_idx] = current_value
-    window_idx = (window_idx + 1) % 3
-
-    window_sum = sum(window)
-    if previous_value < window_sum:
-        counter += 1
-    previous_value = window_sum
-
-answer_b = counter
-print("Part b: " + str(answer_b))
-if args.submit and not args.testcase and answer_b:
-    submit(answer=answer_b, part="b", day=1, year=2021)
+def part_b(data: list[str]):
+    previous_value = float("inf")
+    window = [float("inf"), float("inf"), float("inf")]
+    window_idx = 0
+    counter = 0
+    for line in data:
+        current_value = int(line)
+        window[window_idx] = current_value
+        window_idx = (window_idx + 1) % 3
+        window_sum = sum(window)
+        if previous_value < window_sum:
+            counter += 1
+        previous_value = window_sum
+    return counter
