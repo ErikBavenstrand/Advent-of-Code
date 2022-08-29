8afd1b62f1e4af22fcd4688b8a6e41ee956b791e
Erik Båvenstrand <erik@bavenstrand.se>
2022-08-29 12:21:38 +0200

    Updated some files

--- adventlib/Tree.py
+++ adventlib/Tree.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 
-from abc import ABC, abstractmethod
-from typing import Any, Union
+from abc import ABC
+from typing import Any, Optional, Union
 
 
 class BinaryNode(ABC):
-    def __init__(self, data: Any = None, parent: BinaryNode = None):
+    def __init__(self, data: Any = None, parent: Optional[BinaryNode] = None):
         self.left: Union[BinaryNode, None] = None
         self.right: Union[BinaryNode, None] = None
         self.parent: Union[BinaryNode, None] = parent
@@ -23,11 +23,11 @@ class BinaryNode(ABC):
         lines, *_ = self._display_aux()
         return lines
 
-    def _display_aux(self):
+    def _display_aux(self: BinaryNode):
         """Returns list of strings, width, height, and horizontal coordinate of the root."""
         # No child.
         if self.right is None and self.left is None:
-            line = '%s' % self.value
+            line = "%s" % self.value
             width = len(line)
             height = 1
             middle = width // 2
@@ -36,38 +36,39 @@ class BinaryNode(ABC):
         # Only left child.
         elif self.right is None and self.left:
             lines, n, p, x = self.left._display_aux()
-            s = '%s' % self.value
+            s = "%s" % self.value
             u = len(s)
-            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
-            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
-            shifted_lines = [line + u * ' ' for line in lines]
+            first_line = (x + 1) * " " + (n - x - 1) * "_" + s
+            second_line = x * " " + "/" + (n - x - 1 + u) * " "
+            shifted_lines = [line + u * " " for line in lines]
             return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
 
         # Only right child.
         if self.left is None and self.right:
             lines, n, p, x = self.right._display_aux()
-            s = '%s' % self.value
+            s = "%s" % self.value
             u = len(s)
-            first_line = s + x * '_' + (n - x) * ' '
-            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
-            shifted_lines = [u * ' ' + line for line in lines]
+            first_line = s + x * "_" + (n - x) * " "
+            second_line = (u + x) * " " + "\\" + (n - x - 1) * " "
+            shifted_lines = [u * " " + line for line in lines]
             return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
 
         # Two children.
         if self.left and self.right:
             left, n, p, x = self.left._display_aux()
             right, m, q, y = self.right._display_aux()
-            s = '%s' % self.value
+            s = "%s" % self.value
             u = len(s)
-            first_line = (x + 1) * ' ' + (n - x - 1) * \
-                '_' + s + y * '_' + (m - y) * ' '
-            second_line = x * ' ' + '/' + \
-                (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+            first_line = (x + 1) * " " + (n - x - 1) * "_" + s + y * "_" + (m - y) * " "
+            second_line = (
+                x * " " + "/" + (n - x - 1 + u + y) * " " + "\\" + (m - y - 1) * " "
+            )
             if p < q:
-                left += [n * ' '] * (q - p)
+                left += [n * " "] * (q - p)
             elif q < p:
-                right += [m * ' '] * (p - q)
+                right += [m * " "] * (p - q)
             zipped_lines = zip(left, right)
-            lines = [first_line, second_line] + \
-                [a + u * ' ' + b for a, b in zipped_lines]
+            lines = [first_line, second_line] + [
+                a + u * " " + b for a, b in zipped_lines
+            ]
             return lines, n + m + u, max(p, q) + 2, n + u // 2
