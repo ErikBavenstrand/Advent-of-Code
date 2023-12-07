8afb8925ebdc81e93df1c49d8154748ba385c74e
Erik Båvenstrand <erik@bavenstrand.se>
2023-12-07 08:36:27 +0100

    temp

--- 2023/07/code.py
+++ 2023/07/code.py
@@ -3,14 +3,14 @@
 # URL: https://adventofcode.com/2023/day/7
 
 
-def evaluate_hand(hand: str) -> tuple[int, int]:
-    """Get the value of a camel poker hand.
+def get_tie_break_value(hand: str) -> int:
+    """Get the tie break value of a camel poker hand.
 
     Args:
         hand: A poker hand.
 
     Returns:
-        The value of the hand.
+        The tie break value of the hand.
     """
     card_values = {
         "A": "14",
@@ -27,36 +27,48 @@ def evaluate_hand(hand: str) -> tuple[int, int]:
         "3": "03",
         "2": "02",
     }
+    return int("".join(card_values[card] for card in hand))
+
+
+def evaluate_hand(hand: str) -> int:
+    """Get the value of a camel poker hand.
+
+    Args:
+        hand: A poker hand.
+
+    Returns:
+        The value of the hand.
+    """
+
     unique_cards_count = len(set(hand))
     counts = [hand.count(card) for card in set(hand)]
-    tie_break_value = int("".join(card_values[card] for card in hand))
 
     # Five of a kind
     if unique_cards_count == 1 and 5 in counts:
-        return 7, tie_break_value
+        return 7
 
     # Four of a kind
     if unique_cards_count == 2 and 4 in counts:
-        return 6, tie_break_value
+        return 6
 
     # Full house
     if unique_cards_count == 2 and 3 in counts and 2 in counts:
-        return 5, tie_break_value
+        return 5
 
     # Three of a kind
     if unique_cards_count == 3 and 3 in counts:
-        return 4, tie_break_value
+        return 4
 
     # Two pairs
     if unique_cards_count == 3 and counts.count(2) == 2:
-        return 3, tie_break_value
+        return 3
 
     # One pair
     if unique_cards_count == 4 and 2 in counts:
-        return 2, tie_break_value
+        return 2
 
     # High card
-    return 1, tie_break_value
+    return 1
 
 
 def part_a(data: list[str]) -> int | str | None:
@@ -71,66 +83,11 @@ def part_a(data: list[str]) -> int | str | None:
     hand_values: list[tuple[str, str, int, int]] = []
     for line in data:
         hand, bid = line.split()
-        hand_type, hand_value = evaluate_hand(hand)
-        hand_values.append((hand, bid, hand_type, hand_value))
+        tie_break_value = get_tie_break_value(hand)
+        hand_value = evaluate_hand(hand)
+        hand_values.append((hand, bid, hand_value, tie_break_value))
     hand_values.sort(key=lambda x: (x[2], x[3]))
-    return sum(int(bid) * (i + 1) for i, (_, bid, _, _) in enumerate(hand_values))
-
-
-def evaluate_hand_part_b(hand: str) -> tuple[int, int]:
-    """Get the value of a camel poker hand.
-
-    Args:
-        hand: A poker hand.
-
-    Returns:
-        The value of the hand.
-    """
-    card_values = {
-        "A": "14",
-        "K": "13",
-        "Q": "12",
-        "T": "11",
-        "9": "10",
-        "8": "09",
-        "7": "08",
-        "6": "07",
-        "5": "06",
-        "4": "05",
-        "3": "04",
-        "2": "03",
-        "J": "02",
-    }
-    unique_cards_count = len(set(hand))
-    counts = [hand.count(card) for card in set(hand)]
-    tie_break_value = int("".join(card_values[card] for card in hand))
-
-    # Five of a kind
-    if (unique_cards_count == 1 and 5 in counts) or ():
-        return 7, tie_break_value
-
-    # Four of a kind
-    if unique_cards_count == 2 and 4 in counts:
-        return 6, tie_break_value
-
-    # Full house
-    if unique_cards_count == 2 and 3 in counts and 2 in counts:
-        return 5, tie_break_value
-
-    # Three of a kind
-    if unique_cards_count == 3 and 3 in counts:
-        return 4, tie_break_value
-
-    # Two pairs
-    if unique_cards_count == 3 and counts.count(2) == 2:
-        return 3, tie_break_value
-
-    # One pair
-    if unique_cards_count == 4 and 2 in counts:
-        return 2, tie_break_value
-
-    # High card
-    return 1, tie_break_value
+    return sum(int(bid) * i for i, (_, bid, _, _) in enumerate(hand_values, start=1))
 
 
 def part_b(data: list[str]) -> int | str | None:
@@ -142,4 +99,29 @@ def part_b(data: list[str]) -> int | str | None:
     Returns:
         Solution to the challenge.
     """
+    from collections import Counter
+
+    order = "J23456789TQKA"
+    my = 251156055
+    hi = 251481660
+    L = [line.split() for line in data]
+    L.sort(key=lambda x: [order.index(c) for c in x[0]])
+    L.sort(
+        key=lambda x: max(
+            sorted(Counter(x[0].replace("J", o)).values())[::-1] for o in order
+        )
+    )
+
+    print(sum(i * int(bid) for i, (_, bid) in enumerate(L, start=1)))
+
     return None
+    hand_values: list[tuple[str, str, int, int]] = []
+    for line in data:
+        hand, bid = line.split()
+        tie_break_value = get_tie_break_value(hand)
+        hand_value = max(
+            evaluate_hand(hand.replace("J", card)) for card in "J23456789TQKA"
+        )
+        hand_values.append((hand, bid, hand_value, tie_break_value))
+    hand_values.sort(key=lambda x: (x[2], x[3]))
+    return sum(int(bid) * i for i, (_, bid, _, _) in enumerate(hand_values, start=1))
